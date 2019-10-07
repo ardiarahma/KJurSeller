8fdf5313a62973f5390460ccd9bf2f4e3dc5dda4
Kafa <afyadkf@gmail.com>
2019-10-07 22:38:50 +0700

    Add Product Activity, Some Intent

--- app/src/main/java/id/technow/kjurseller/LiveSalesActivity.java
+++ app/src/main/java/id/technow/kjurseller/LiveSalesActivity.java
@@ -1,7 +1,10 @@
 package id.technow.kjurseller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class LiveSalesActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class LiveSalesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_sales);
+
+        LinearLayout btnLiveReport = findViewById(R.id.btnLiveReport);
+        btnLiveReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LiveSalesActivity.this, LocLiveReportActivity.class));
+            }
+        });
+
+        LinearLayout btnUpdateStock = findViewById(R.id.btnUpdateStock);
+        btnUpdateStock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LiveSalesActivity.this, LocUpdateStockActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/id/technow/kjurseller/MainActivity.java
+++ app/src/main/java/id/technow/kjurseller/MainActivity.java
@@ -1,13 +1,69 @@
 package id.technow.kjurseller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView btnSettings;
+    private LinearLayout btnBalance, btnOpenStore, btnLiveSales, btnCloseStore, btnReport;
+    private TextView txtUsername, txtEmail, txtBalance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+        btnBalance = findViewById(R.id.btnBalance);
+        /*
+        btnBalance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+        */
+        btnOpenStore = findViewById(R.id.btnOpenStore);
+        btnOpenStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LocOpenStoreActivity.class));
+            }
+        });
+        btnLiveSales = findViewById(R.id.btnLiveSales);
+        btnLiveSales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LiveSalesActivity.class));
+            }
+        });
+        btnCloseStore = findViewById(R.id.btnCloseStore);
+        btnCloseStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LocCloseStoreActivity.class));
+            }
+        });
+        btnReport = findViewById(R.id.btnReport);
+        btnOpenStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ReportActivity.class));
+            }
+        });
+        txtUsername = findViewById(R.id.txtUsername);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtBalance = findViewById(R.id.txtBalance);
     }
 }
