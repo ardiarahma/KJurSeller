10e801176f9e1f1b044eec57f52caf81e8593752
Kafa <afyadkf@gmail.com>
2019-10-09 17:15:14 +0700

    Add Wallet and Withdraw Activity, Backend Report Activity

--- app/src/main/java/id/technow/kjurseller/ReportActivity.java
+++ app/src/main/java/id/technow/kjurseller/ReportActivity.java
@@ -1,13 +1,235 @@
 package id.technow.kjurseller;
 
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import id.technow.kjurseller.adapter.ReportAdapter;
+import id.technow.kjurseller.api.RetrofitClient;
+import id.technow.kjurseller.model.ReportList;
+import id.technow.kjurseller.model.ReportListResponse;
+import id.technow.kjurseller.model.User;
+import id.technow.kjurseller.storage.SharedPrefManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ReportActivity extends AppCompatActivity {
+    private TextView dateFrom, dateUntil, txtRStock, txtRSold, txtRRemain, txtRLocation, txtRItem, txtRIncome;
+    private LinearLayout layoutResult;
+    private int pYear, pMonth, pDay;
+    private ArrayList<ReportList> reportList;
+    private RecyclerView recyclerView;
+    private ReportAdapter rAdapter;
+    Context mContext;
+    private SwipeRefreshLayout swipeRefresh;
+    ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        mContext = this;
+
+        dateFrom = findViewById(R.id.dateFrom);
+        dateUntil = findViewById(R.id.dateUntil);
+        txtRStock = findViewById(R.id.txtRStock);
+        txtRSold = findViewById(R.id.txtRSold);
+        txtRRemain = findViewById(R.id.txtRRemain);
+        txtRLocation = findViewById(R.id.txtRLocation);
+        txtRItem = findViewById(R.id.txtRItem);
+        txtRIncome = findViewById(R.id.txtRIncome);
+
+        layoutResult = findViewById(R.id.layoutResult);
+
+        recyclerView = findViewById(R.id.listReport);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDate = sdf.format(new Date().getTime());
+
+        dateFrom.setText(currentDate);
+        dateUntil.setText(currentDate);
+
+        dateFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                pYear = c.get(Calendar.YEAR);
+                pMonth = c.get(Calendar.MONTH);
+                pDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog.OnDateSetListener pDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        pYear = year;
+                        pMonth = monthOfYear+1;
+                        pDay = dayOfMonth;
+
+                        String fm = "" + pMonth;
+                        String fd = "" + pDay;
+                        if(pMonth < 10){
+                            fm = "0" + pMonth;
+                        }
+                        if (pDay < 10){
+                            fd = "0" + pDay;
+                        }
+
+                        dateFrom.setText(pYear+"-"+fm+"-"+fd);
+                    }
+                };
+                DatePickerDialog dialog = new DatePickerDialog(mContext, pDateSetListener, pYear, pMonth, pDay);
+                dialog.getDatePicker().setMaxDate(new Date().getTime());
+                dialog.show();
+            }
+        });
+
+        dateUntil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                pYear = c.get(Calendar.YEAR);
+                pMonth = c.get(Calendar.MONTH);
+                pDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog.OnDateSetListener pDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        pYear = year;
+                        pMonth = monthOfYear+1;
+                        pDay = dayOfMonth;
+
+                        String fm = "" + pMonth;
+                        String fd = "" + pDay;
+                        if(pMonth < 10){
+                            fm = "0" + pMonth;
+                        }
+                        if (pDay < 10){
+                            fd = "0" + pDay;
+                        }
+
+                        dateUntil.setText(pYear+"-"+fm+"-"+fd);
+                    }
+                };
+                DatePickerDialog dialog = new DatePickerDialog(mContext, pDateSetListener, pYear, pMonth, pDay);
+                String strDate = dateFrom.getText().toString();
+                SimpleDateFormat dateFromFormat = new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    Date date = dateFromFormat.parse(strDate);
+                    long mills = date.getTime();
+                    dialog.getDatePicker().setMinDate(mills);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                /*
+                int dateUntilChangedYear = Integer.parseInt(dateUntil.getText().toString().substring(0,4));
+                int dateUntilChangedMonth = Integer.parseInt(dateUntil.getText().toString().substring(5,7));
+                int dateUntilChangedDay = Integer.parseInt(dateUntil.getText().toString().substring(8,10));
+                dialog.getDatePicker().updateDate(dateUntilChangedYear, dateUntilChangedMonth, dateUntilChangedDay);
+                */
+                dialog.getDatePicker().setMaxDate(new Date().getTime());
+                dialog.show();
+            }
+        });
+
+        Button btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                loading = ProgressDialog.show(mContext, null, getString(R.string.please_wait), true, false);
+                reportList();
+            }
+        });
+
+        swipeRefresh = findViewById(R.id.swipeRefresh);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (rAdapter != null) {
+                    rAdapter.refreshEvents(reportList);
+                }
+                reportList();
+            }
+        });
+    }
+
+    private void reportList() {
+        String fromDate = dateFrom.getText().toString();
+        String untilDate = dateUntil.getText().toString();
+
+        User user = SharedPrefManager.getInstance(this).getUser();
+        String token = user.getToken();
+        Call<ReportListResponse> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .reportList("Bearer " + token, fromDate, untilDate);
+
+        call.enqueue(new Callback<ReportListResponse>() {
+
+            @Override
+            public void onResponse(Call<ReportListResponse> call, Response<ReportListResponse> response) {
+                loading.dismiss();
+                if (response.isSuccessful()) {
+                    ReportListResponse reportListResponse = response.body();
+                    if (reportListResponse.getStatus().equals("success")) {
+                        layoutResult.setVisibility(View.VISIBLE);
+
+                        txtRStock.setText(reportListResponse.getStock());
+                        txtRSold.setText(String.valueOf(reportListResponse.getSold()));
+                        txtRRemain.setText(String.valueOf(reportListResponse.getRemain()));
+                        txtRLocation.setText(String.valueOf(reportListResponse.getLocation()));
+                        txtRItem.setText(String.valueOf(reportListResponse.getItem()));
+                        txtRIncome.setText(String.valueOf(reportListResponse.getIncome()));
+                        reportList = reportListResponse.getReportList();
+                        rAdapter = new ReportAdapter(reportList);
+                        RecyclerView.LayoutManager eLayoutManager = new LinearLayoutManager(getApplicationContext());
+                        recyclerView.setLayoutManager(eLayoutManager);
+                        recyclerView.setItemAnimator(new DefaultItemAnimator());
+                        recyclerView.setAdapter(rAdapter);
+                        if (eLayoutManager.getItemCount() == 0) {
+                            //Do something
+                        }
+                    }
+                }
+                swipeRefresh.setRefreshing(false);
+            }
+
+            @Override
+            public void onFailure(Call<ReportListResponse> call, Throwable t) {
+                loading.dismiss();
+                swipeRefresh.setRefreshing(false);
+                layoutResult.setVisibility(View.GONE);
+                Log.d("TAG", "Response = " + t.toString());
+            }
+        });
     }
 }
---
+++ app/src/main/java/id/technow/kjurseller/WithdrawActivity.java
@@ -0,0 +1,175 @@
+package id.technow.kjurseller;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.BigInteger;
+
+import id.technow.kjurseller.api.RetrofitClient;
+import id.technow.kjurseller.model.DetailUserResponse;
+import id.technow.kjurseller.model.User;
+import id.technow.kjurseller.model.WithdrawResponse;
+import id.technow.kjurseller.storage.SharedPrefManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class WithdrawActivity extends AppCompatActivity {
+    private TextView txtNoInfo, txtHolderName, txtHolderBankNumber, txtBalance;
+    private EditText edtAmount;
+    private Button btnWithdraw;
+    private LinearLayout layoutBankInfo;
+    Context mContext;
+    ProgressDialog loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw);
+
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        mContext = this;
+
+        layoutBankInfo = findViewById(R.id.layoutBankInfo);
+        txtNoInfo = findViewById(R.id.txtNoInfo);
+        txtHolderName = findViewById(R.id.txtHolderName);
+        txtHolderBankNumber = findViewById(R.id.txtHolderBankNumber);
+        txtBalance = findViewById(R.id.txtBalance);
+        edtAmount = findViewById(R.id.etAmount);
+        btnWithdraw = findViewById(R.id.btnWithdraw);
+
+        layoutBankInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(WithdrawActivity.this, SettingsBankInfoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnWithdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading = ProgressDialog.show(mContext, null, getString(R.string.please_wait), true, false);
+                withdraw();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        loading = ProgressDialog.show(mContext, null, getString(R.string.please_wait), true, false);
+        detailUser();
+    }
+
+    private void detailUser() {
+        String accept = "application/json";
+
+        User user = SharedPrefManager.getInstance(this).getUser();
+        String token = user.getToken();
+        Call<DetailUserResponse> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .detailUser("Bearer " + token, accept);
+
+        call.enqueue(new Callback<DetailUserResponse>() {
+
+            @Override
+            public void onResponse(Call<DetailUserResponse> call, Response<DetailUserResponse> response) {
+                loading.dismiss();
+                btnWithdraw.setEnabled(true);
+                if (response.isSuccessful()) {
+                    DetailUserResponse detailUserResponse = response.body();
+                    if (detailUserResponse.getStatus().equals("success")) {
+                        txtBalance.setText(String.valueOf(detailUserResponse.getDetailUser().getBalance()));
+                        BigInteger bankAccNumber = detailUserResponse.getDetailUser().getDetailStore().getDetailBankAccount().getBankAccNumber();
+                        if (bankAccNumber != null) {
+                            txtNoInfo.setVisibility(View.GONE);
+                            txtHolderBankNumber.setVisibility(View.VISIBLE);
+                            txtHolderBankNumber.setText(String.valueOf(bankAccNumber));
+                        }
+                        String bankHolderName = detailUserResponse.getDetailUser().getDetailStore().getDetailBankAccount().getBankHolderName();
+                        if (bankHolderName != null) {
+                            txtNoInfo.setVisibility(View.GONE);
+                            txtHolderName.setVisibility(View.VISIBLE);
+                            txtHolderName.setText(bankHolderName);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DetailUserResponse> call, Throwable t) {
+                loading.dismiss();
+                btnWithdraw.setEnabled(false);
+                Toast.makeText(mContext, "Can't get bank information. Try again later", Toast.LENGTH_LONG).show();
+                Log.d("TAG", "Response = " + t.toString());
+            }
+        });
+    }
+
+    private void withdraw() {
+        String amountET = edtAmount.getText().toString();
+
+        if (amountET.isEmpty()) {
+            loading.dismiss();
+            edtAmount.setError("Withdraw amount is required");
+            edtAmount.requestFocus();
+            return;
+        }
+
+        int amount = Integer.parseInt(amountET);
+
+        User user = SharedPrefManager.getInstance(this).getUser();
+        String token = user.getToken();
+        Call<WithdrawResponse> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .withdraw("Bearer " + token, amount);
+
+        call.enqueue(new Callback<WithdrawResponse>() {
+
+            @Override
+            public void onResponse(Call<WithdrawResponse> call, Response<WithdrawResponse> response) {
+                loading.dismiss();
+                if (response.isSuccessful()) {
+                    WithdrawResponse withdrawResponse = response.body();
+                    if (withdrawResponse.getStatus().equals("success")) {
+                        Toast.makeText(mContext, "Success", Toast.LENGTH_LONG).show();
+                        onBackPressed();
+                    }
+                    else {
+                        Toast.makeText(mContext, withdrawResponse.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WithdrawResponse> call, Throwable t) {
+                loading.dismiss();
+                btnWithdraw.setEnabled(false);
+                Toast.makeText(mContext, "Something wrong. Try again later", Toast.LENGTH_LONG).show();
+                Log.d("TAG", "Response = " + t.toString());
+            }
+        });
+    }
+}
