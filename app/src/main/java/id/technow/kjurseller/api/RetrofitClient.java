7e867d31e189bc948cc5c7b51aa612af07f72d89
madypamungkas <madypamungkas@gmail.com>
2019-12-02 11:16:42 +0700

    change https

--- app/src/main/java/id/technow/kjurseller/api/RetrofitClient.java
+++ app/src/main/java/id/technow/kjurseller/api/RetrofitClient.java
@@ -11,7 +11,7 @@ public class RetrofitClient {
     //private static final String BASE_URL_API = "http://10.33.194.150/k-jur/public/api/penjual/";
     //private static final String BASE_URL_API = "http://10.0.2.2:8000/api/penjual/";
     //private static final String BASE_URL_API = "http://k-jur.apotech.id/api/penjual/";
-    private static final String BASE_URL_API = "http://k-jur.technow.id/api/penjual/";
+    private static final String BASE_URL_API = "https://k-jur.technow.id/api/penjual/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
